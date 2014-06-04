d1e0a2e3443bcc7fbcf49d68c5a4e56bccc11ad1
cheney <wancj17@gmail.com>
2014-06-04 17:29:49 +0800

    fix #77 , Ng-Repeat in Overlay support nested overlay

--- src/js/lib/overlay.js
+++ src/js/lib/overlay.js
@@ -12,7 +12,8 @@ angular.module('mobile-angular-ui.directives.overlay', []).directive('overlay', 
 
         var html = "<div class=\"overlay\" id=\"" + id + "\" toggleable " + active + " parent-active-class=\"overlay-in\" active-class=\"overlay-show\">\n  <div class=\"overlay-inner\">\n    <div class=\"overlay-background\"></div>\n    <a href=\"#" + id + "\" toggle=\"off\" class=\"overlay-dismiss\">\n      <i class=\"fa fa-times-circle-o\"></i>\n    </a>\n    <div class=\"overlay-content\">\n      <div class=\"overlay-body\">\n        " + body + "\n      </div>\n    </div>\n  </div>\n</div>";
         elem.remove();
-
+        html = html.replace(/repeat-hack/g, 'data-ng-repeat');
+        
         var sameId = angular.element(document.getElementById(id));
 
         if (sameId.length > 0 && sameId.hasClass('overlay')) {
